b699a0b99c16a745660be3e1469db9cec0760d92
Shin seongsu <tlstjdtn321@naver.com>
2022-01-23 23:35:06 +0900

    spring batch db 연결

---
+++ 스프링 배치/Hello SpringBatch/src/main/java/io/springbatch/springbatchlecture/config/DBJobConfiguration.java
@@ -0,0 +1,55 @@
+package io.springbatch.springbatchlecture.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class DBJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step1 was executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step2 was executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+}
--- 스프링 배치/Hello SpringBatch/src/main/java/io/springbatch/springbatchlecture/config/HelloJobConfiguration.java
+++ 스프링 배치/Hello SpringBatch/src/main/java/io/springbatch/springbatchlecture/config/HelloJobConfiguration.java
@@ -16,49 +16,49 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HelloJobConfiguration {
 
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-    public Job helloJob() {
-        return jobBuilderFactory.get("helloJob")
-                .start(helloStep1())
-                .next(helloStep2())
-                .build();
-    }
-
-    @Bean
-    public Step helloStep1() {
-        return stepBuilderFactory.get("helloStep1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-
-                        System.out.println("========================");
-                        System.out.println(" >> Hello Spring Batch!!");
-                        System.out.println("========================");
-
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .build();
-    }
-
-    @Bean
-    public Step helloStep2() {
-        return stepBuilderFactory.get("helloStep2")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-
-                        System.out.println("========================");
-                        System.out.println(" >> step2 was executed");
-                        System.out.println("========================");
-
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .build();
-    }
+//    private final JobBuilderFactory jobBuilderFactory;
+//    private final StepBuilderFactory stepBuilderFactory;
+//
+//    @Bean
+//    public Job helloJob() {
+//        return jobBuilderFactory.get("helloJob")
+//                .start(helloStep1())
+//                .next(helloStep2())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step helloStep1() {
+//        return stepBuilderFactory.get("helloStep1")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//
+//                        System.out.println("========================");
+//                        System.out.println(" >> Hello Spring Batch!!");
+//                        System.out.println("========================");
+//
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                })
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step helloStep2() {
+//        return stepBuilderFactory.get("helloStep2")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//
+//                        System.out.println("========================");
+//                        System.out.println(" >> step2 was executed");
+//                        System.out.println("========================");
+//
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                })
+//                .build();
+//    }
 
 }
