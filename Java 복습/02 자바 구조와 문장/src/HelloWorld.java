5e22e0970c0f10e174a7e2204787c9357eb037d7
Shin seongsu <tlstjdtn321@naver.com>
2022-04-04 22:21:30 +0900

    :fire: 자바 구조와 문장

---
+++ Java 복습/02 자바 구조와 문장/src/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+
+    int age;
+    //int 0age; // 안됨
+    int $age;
+    int _age;
+    int user_age;
+    int 나이;
+
+    public static void main(String[] args)
+    {
+       String helloWorld = "Hello Wordl";
+        System.out.println(helloWorld);
+    }
+
+}
+
+class HelloWorld2 {
+
+}
