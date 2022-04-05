b4f8512a3e53ec454cc0debd702f6e247f678864
Shin seongsu <tlstjdtn321@naver.com>
2022-04-05 22:34:50 +0900

    :fire: 실수 자료형

---
+++ Java 복습/06 실수 자료형/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args ){
+
+        double d1 = 123.456789;
+        double d2 = 1.23456789E2;
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+    }
+
+}
