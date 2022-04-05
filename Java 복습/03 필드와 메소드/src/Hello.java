f746b7e33bdbc5623b906faeeeb336fa2ae9bf1c
Shin seongsu <tlstjdtn321@naver.com>
2022-04-05 22:33:03 +0900

    :fire: 필드와 메소드

---
+++ Java 복습/03 필드와 메소드/src/Hello.java
@@ -0,0 +1,31 @@
+public class Hello {
+
+    // 필드 정의
+    int age;
+
+    // int -> 키워드
+    // age -> 식별자(변수)
+
+    int myAge = 10;
+
+    // 메서드 (함수)
+
+    // 두개의 숫자값을 입력받아서 높은 값을 구하는 메서드
+    // 매개변수 (필수는 아니다.)
+    // 리턴타입 (필수는 아니다.)
+    // f(x) = x + y;
+
+    // 함수를 선언하는 부분
+    int maxNumber(int x, int y) {
+        return 0;
+    }
+
+    // 리턴 타입이 없는 것을 만들려면 void를 넣는다.
+    void maxNumber() {
+
+        // 함수를 호출하는 방법
+        int result = maxNumber(10, 20);
+    }
+
+
+}
