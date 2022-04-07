62f99840dcee50e0c2e26aa692a1889fdfe884b8
Shin seongsu <tlstjdtn321@naver.com>
2022-04-07 18:10:20 +0900

    :rocket: A+B - 2

---
+++ 백준문제풀기/src/bronze5/problem2558/Main.java
@@ -0,0 +1,19 @@
+package bronze5.problem2558;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(a + b));
+        bw.flush();
+        br.close();
+        bw.close();
+
+    }
+}
