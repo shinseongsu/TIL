e3514c75d110b4ca1085b738d6aed6964f6a0143
Shin seongsu <tlstjdtn321@naver.com>
2022-04-07 12:59:56 +0900

    :fire: 증간연산자

---
+++ Java 복습/12 증감연산자/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i21 = 10;
+        System.out.println(i21);
+
+        // 증가 연산자
+        ++i21;
+        System.out.println(i21);
+
+        // 전위 연산자
+        i21++;
+        System.out.println(i21);
+
+        System.out.println("-----------------");
+
+        System.out.println(++i21);   // 출력할 때 +1
+        System.out.println(i21++);   // 출력 후,  +1
+
+        System.out.println("-----------------");
+
+        System.out.println(i21);
+
+    }
+
+}
