220e0f37f651b936b7974ae356b2ce6ebdd369d0
Shin seongsu <tlstjdtn321@naver.com>
2022-04-07 18:09:54 +0900

    :rocket: Hello World

---
+++ 백준문제풀기/src/bronze5/problem2557/Main.java
@@ -0,0 +1,7 @@
+package bronze5.problem2557;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ 백준문제풀기/src/bronze5/problem2557/README.md
@@ -0,0 +1,10 @@
+# Hello World
+
+## 문제
+Hello World!를 출력하시오.
+
+
+## 출력
+```
+Hello World!
+```
