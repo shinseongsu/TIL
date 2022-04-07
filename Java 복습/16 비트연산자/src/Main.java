4815dcc55258fdc832ada0d0ac219b64cf1e56b9
Shin seongsu <tlstjdtn321@naver.com>
2022-04-07 22:50:51 +0900

    :fire: 비트연산자

---
+++ Java 복습/16 비트연산자/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i1 = 12;
+        int i2 = ~i1 + 1;
+
+        System.out.println(i1);
+
+        int i3 = 0b0000_0000_0000_0000_0000_0000_0001_0100;
+        int i4 = i1 >> 1;
+
+        System.out.println("i4 = " + i4);
+
+    }
+
+}
