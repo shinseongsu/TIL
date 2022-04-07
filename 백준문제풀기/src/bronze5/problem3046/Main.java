9f51fa7675fc2dffe893b083dd7e5ee72ceb82cc
Shin seongsu <tlstjdtn321@naver.com>
2022-04-08 01:33:24 +0900

    :rocket: R2

---
+++ 백준문제풀기/src/bronze5/problem3046/Main.java
@@ -0,0 +1,15 @@
+package bronze5.problem3046;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int r1 = sc.nextInt();
+        int s = sc.nextInt();
+
+        System.out.println( 2 * s - r1 );
+
+    }
+}
