1ced900d4cb4b3b2d9f3265391db6a7a80df41da
Shin seongsu <tlstjdtn321@naver.com>
2022-04-07 18:08:12 +0900

    :rocket: 16진수

---
+++ 백준문제풀기/src/bronze5/problem1550/Main.java
@@ -0,0 +1,11 @@
+package bronze5.problem1550;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(Integer.parseInt(input,16));
+    }
+}
