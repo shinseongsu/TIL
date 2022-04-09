916f0a5010d832c452807c49a5ed989e8268eb21
Shin seongsu <tlstjdtn321@naver.com>
2022-04-09 22:36:42 +0900

    :rocket: 나머지

---
+++ 백준문제풀기/src/bronze5/problem10430/Main.java
@@ -0,0 +1,19 @@
+package bronze5.problem10430;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println( (a+b) % c );
+        System.out.println( ((a%c) + (b%c)) % c );
+        System.out.println( (a * b) % c );
+        System.out.println( (a%c) * (b%c) %c );
+
+    }
+}
