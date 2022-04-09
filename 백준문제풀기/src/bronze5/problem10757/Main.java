2ea5b222899cdd33b10f5f906a62b8b5fc592079
Shin seongsu <tlstjdtn321@naver.com>
2022-04-09 22:37:27 +0900

    :rocket: 큰 수 A+B

---
+++ 백준문제풀기/src/bronze5/problem10757/Main.java
@@ -0,0 +1,15 @@
+package bronze5.problem10757;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+
+        System.out.println(a.add(b));
+    }
+}
