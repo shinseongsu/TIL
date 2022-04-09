ebf88a82e68e75be203d0d034cff4fbbdb86f880
Shin seongsu <tlstjdtn321@naver.com>
2022-04-09 22:34:06 +0900

    :rocket: Next in line

---
+++ 백준문제풀기/src/bronze5/problem6749/Main.java
@@ -0,0 +1,15 @@
+package bronze5.problem6749;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println( 2*b - a  );
+    }
+
+}
