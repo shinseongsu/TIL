39759be2658a4edf637a4393ad1fdba088f59043
Shin seongsu <tlstjdtn321@naver.com>
2022-04-09 22:37:02 +0900

    :rocket: 오늘날짜

---
+++ 백준문제풀기/src/bronze5/problem10699/Main.java
@@ -0,0 +1,14 @@
+package bronze5.problem10699;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        LocalDate now = LocalDate.now();
+
+        System.out.println(now.toString());
+    }
+}
