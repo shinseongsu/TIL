c1b3a3c7b6600f666562504c637e5fba809248cd
Shin seongsu <tlstjdtn321@naver.com>
2022-04-09 22:32:47 +0900

    :rocket: 콜센터

---
+++ 백준문제풀기/src/bronze5/problem5339/Main.java
@@ -0,0 +1,24 @@
+package bronze5.problem5339;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("     /~\\\n");
+        bw.write("    ( oo|\n");
+        bw.write("    _\\=/_\n");
+        bw.write("   /  _  \\\n");
+        bw.write("  //|/.\\|\\\\\n");
+        bw.write(" ||  \\ /  ||\n");
+        bw.write("============\n");
+        bw.write("|          |\n");
+        bw.write("|          |\n");
+        bw.write("|          |\n");
+
+        bw.flush();
+        bw.close();
+
+    }
+}
