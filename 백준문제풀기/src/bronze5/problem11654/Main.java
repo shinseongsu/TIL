07be396e6d0838c8ebb8b8f9f284caaa265a056e
Shin seongsu <tlstjdtn321@naver.com>
2022-04-09 22:39:25 +0900

    :rocket: 아스키 코드

---
+++ 백준문제풀기/src/bronze5/problem11654/Main.java
@@ -0,0 +1,11 @@
+package bronze5.problem11654;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println((int) sc.next().charAt(0));
+    }
+}
