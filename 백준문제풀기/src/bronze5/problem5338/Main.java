2274806d1d429a911183f3d9a535b3e47dc749ff
Shin seongsu <tlstjdtn321@naver.com>
2022-04-09 22:32:22 +0900

    :rocket: 마이크로소프트 로고

---
+++ 백준문제풀기/src/bronze5/problem5338/Main.java
@@ -0,0 +1,11 @@
+package bronze5.problem5338;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.print(  "'-..-'|_.-''-._|");
+    }
+}
