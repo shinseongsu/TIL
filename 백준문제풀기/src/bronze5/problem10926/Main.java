56d1b8683e430696a6b292a25d1f19f8811226f3
Shin seongsu <tlstjdtn321@naver.com>
2022-04-09 22:38:20 +0900

    :rocket: ??

---
+++ 백준문제풀기/src/bronze5/problem10926/Main.java
@@ -0,0 +1,13 @@
+package bronze5.problem10926;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String id = sc.next();
+
+        System.out.println(id + "??!");
+    }
+}
