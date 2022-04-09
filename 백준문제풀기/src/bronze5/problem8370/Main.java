20129a61f87566e9a243e0ab8b515baebefcba8e
Shin seongsu <tlstjdtn321@naver.com>
2022-04-09 22:35:17 +0900

    :rocket: Plane

---
+++ 백준문제풀기/src/bronze5/problem8370/Main.java
@@ -0,0 +1,18 @@
+package bronze5.problem8370;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int aw = sc.nextInt();
+        int ah = sc.nextInt();
+
+        int bw = sc.nextInt();
+        int bh = sc.nextInt();
+
+        System.out.println( (aw * ah) + (bw * bh) );
+
+    }
+}
