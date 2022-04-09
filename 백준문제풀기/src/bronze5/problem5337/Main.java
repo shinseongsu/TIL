b30391241612532bba9cc160d16d90daa0f84e0e
Shin seongsu <tlstjdtn321@naver.com>
2022-04-09 22:31:53 +0900

    :rocket: 웰컴

---
+++ 백준문제풀기/src/bronze5/problem5337/Main.java
@@ -0,0 +1,9 @@
+package bronze5.problem5337;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.print("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
