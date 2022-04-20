d7048518f17f557b02a1baf1d0a8c3cf7ef9c28e
Shin seongsu <tlstjdtn321@naver.com>
2022-04-20 22:12:24 +0900

    :star: 1,2,3 더하기 3

---
+++ 코드플러스/알고리즘기초-1/연습/src/problem01/Main.java
@@ -0,0 +1,26 @@
+package problem01;
+
+import java.util.Scanner;
+
+public class Main {
+    static final long mod = 1000000009L;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long[] d = new long[1000001];
+        d[0] = 1;
+        for(int i = 1; i <= 1000000; i++) {
+            for(int j = 1 ; j <= 3 ; j++) {
+                if(i-j >= 0) {
+                    d[i] += d[i-j];
+                }
+            }
+            d[i] %= mod;
+        }
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            System.out.println(d[n]);
+        }
+
+    }
+}
