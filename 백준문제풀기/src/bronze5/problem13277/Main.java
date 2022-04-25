5b981a96f1bd437a7627be98278fd996fcf6cfb7
Shin seongsu <tlstjdtn321@naver.com>
2022-04-26 00:28:29 +0900

    :star: 큰 수 곱셈

---
+++ 백준문제풀기/src/bronze5/problem13277/Main.java
@@ -0,0 +1,16 @@
+package bronze5.problem13277;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+
+        System.out.print(a.multiply(b));
+
+    }
+}
