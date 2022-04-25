000db31546175e4693d113685e5316afed79b8da
Shin seongsu <tlstjdtn321@naver.com>
2022-04-26 00:28:48 +0900

    :star: 나는 행복합니다~

---
+++ 백준문제풀기/src/bronze5/problem14652/Main.java
@@ -0,0 +1,16 @@
+package bronze5.problem14652;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sit = sc.nextInt();
+
+        System.out.print(sit / b  + " " + sit % b );
+
+    }
+}
