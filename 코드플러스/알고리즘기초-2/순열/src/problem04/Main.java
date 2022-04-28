dd3bb12c94d1cb8769558693533a49d73adec712
Shin seongsu <tlstjdtn321@naver.com>
2022-04-29 00:44:25 +0900

    :fire: 차이를 최대로

---
+++ 코드플러스/알고리즘기초-2/순열/src/problem04/Main.java
@@ -0,0 +1,58 @@
+package problem04;
+
+import java.util.*;
+
+public class Main {
+    public static boolean next_permutation(int[] a) {
+        int i = a.length-1;
+        while (i > 0 && a[i-1] >= a[i]) {
+            i -= 1;
+        }
+
+        if (i <= 0) {
+            return false;
+        }
+
+        int j = a.length-1;
+        while (a[j] <= a[i-1]) {
+            j -= 1;
+        }
+
+        int temp = a[i-1];
+        a[i-1] = a[j];
+        a[j] = temp;
+
+        j = a.length-1;
+        while (i < j) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i += 1;
+            j -= 1;
+        }
+        return true;
+    }
+    public static int calculate(int a[]) {
+        int sum = 0;
+        for (int i=1; i<a.length; i++) {
+            sum += Math.abs(a[i] - a[i-1]);
+        }
+        return sum;
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i=0; i<n; i++) {
+            a[i] = sc.nextInt();
+        }
+        Arrays.sort(a);
+        int ans = 0;
+        do {
+            int temp = calculate(a);
+            ans = Math.max(ans, temp);
+        } while(next_permutation(a));
+
+        System.out.println(ans);
+    }
+}
