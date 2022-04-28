5d79cb6fa1bc9952fbacb4ac07f843824918a5d7
Shin seongsu <tlstjdtn321@naver.com>
2022-04-29 00:00:29 +0900

    :fire: N과 M(8)

---
+++ 코드플러스/알고리즘기초-2/N과 M/src/problem08/Main.java
@@ -0,0 +1,38 @@
+package problem08;
+
+import java.util.*;
+public class Main {
+    static boolean[] c = new boolean[10];
+    static int[] a = new int[10];
+    static int[] num = new int[10];
+    static StringBuilder go(int index, int start, int n, int m) {
+        if (index == m) {
+            StringBuilder sb = new StringBuilder();
+            for (int i=0; i<m; i++) {
+                sb.append(num[a[i]]);
+                if (i != m-1) sb.append(" ");
+            }
+            sb.append("\n");
+            return sb;
+        }
+        StringBuilder ans = new StringBuilder();
+        for (int i=start; i<n; i++) {
+            //if (c[i]) continue;
+            c[i] = true;
+            a[index] = i;
+            ans.append(go(index+1, i, n, m));
+            c[i] = false;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for (int i=0; i<n; i++) {
+            num[i] = sc.nextInt();
+        }
+        Arrays.sort(num, 0, n);
+        System.out.print(go(0,0,n,m));
+    }
+}
