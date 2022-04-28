6313f969eabd10db9b7442f371c723dcca27d2ef
Shin seongsu <tlstjdtn321@naver.com>
2022-04-29 00:45:09 +0900

    :fire: 로또

---
+++ 코드플러스/알고리즘기초-2/순열/src/problem06/Main.java
@@ -0,0 +1,84 @@
+package problem06;
+
+import java.util.*;
+public class Main {
+    static boolean next_permutgetion(int[] a) {
+        int i = a.length-1;
+        while (i > 0 && a[i-1] >= a[i]) {
+            i -= 1;
+        }
+
+        if (i <= 0) {
+            return false;
+        }
+
+        int j = a.length-1;
+        while (a[j] <= a[i-1]) {
+            j -= 1;
+        }
+
+        int temp = a[i-1];
+        a[i-1] = a[j];
+        a[j] = temp;
+
+        j = a.length-1;
+        while (i < j) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i += 1;
+            j -= 1;
+        }
+        return true;
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) break;
+            int[] a = new int[n];
+            for (int i=0; i<n; i++) {
+                a[i] = sc.nextInt();
+            }
+            int[] d = new int[n];
+            for (int i=0; i<n; i++) {
+                if (i < n-6) d[i] = 0;
+                else d[i] = 1;
+            }
+            ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+            do {
+                ArrayList<Integer> cur = new ArrayList<>();
+                for (int i=0; i<n; i++) {
+                    if (d[i] == 1) {
+                        cur.add(a[i]);
+                    }
+                }
+                ans.add(cur);
+            } while (next_permutgetion(d));
+            Collections.sort(ans, new Comparator<ArrayList<Integer>>() {
+                public int compare(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+                    int n = l1.size();
+                    int m = l2.size();
+                    int i = 0;
+                    while (i < n && i < m) {
+                        int t1 = l1.get(i);
+                        int t2 = l2.get(i);
+                        if (t1 < t2) return -1;
+                        else if (t1 > t2) return 1;
+                        i += 1;
+                    }
+                    if (i == n && i != m) return -1;
+                    else if (i != n && i == m) return 1;
+                    return 0;
+                }
+            });
+            for (ArrayList<Integer> v : ans) {
+                for (int x : v) {
+                    System.out.print(x + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
