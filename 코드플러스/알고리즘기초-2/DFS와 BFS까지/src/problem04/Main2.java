fceba81f071ef32acaf51479703b9f86be4a5502
Shin seongsu <tlstjdtn321@naver.com>
2022-05-05 00:52:42 +0900

    :fire: 이분 그래프

---
+++ 코드플러스/알고리즘기초-2/DFS와 BFS까지/src/problem04/Main2.java
@@ -0,0 +1,53 @@
+package problem04;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main2 {
+    public static boolean dfs(ArrayList<Integer>[] a, int[] color, int x, int c) {
+        color[x] = c;
+        for (int y: a[x]) {
+            if (color[y] == 0) {
+                if (dfs(a, color, y, 3-c) == false) {
+                    return false;
+                }
+            } else if (color[y] == color[x]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t =sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            ArrayList<Integer>[] a = (ArrayList<Integer>[]) new ArrayList[n+1];
+            for (int i=1; i<=n; i++) {
+                a[i] = new ArrayList<Integer>();
+            }
+            for(int i=0; i<m; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                a[u].add(v);
+                a[v].add(u);
+            }
+            int[] color = new int[n+1];
+            boolean ok = true;
+            for (int i=1; i<=n; i++) {
+                if (color[i] == 0) {
+                    if(dfs(a, color, i, 1) == false) {
+                        ok = false;
+                    }
+                }
+            }
+            if (ok) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
