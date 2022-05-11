15c2a34e7c084ea4b9e9986b83801ae6694043d4
Shin seongsu <tlstjdtn321@naver.com>
2022-05-12 01:02:13 +0900

    :fire: 철수와 영희

---
+++ 코테해설/src/problem14/Main.java
@@ -0,0 +1,64 @@
+package problem14;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public int solution(int x1, int y1, int x2, int y2) {
+        Set<List<Integer>> setA = new HashSet<>();
+        Set<List<Integer>> setB = new HashSet<>();
+        setA.add(List.of(x1, y1));
+        setB.add(List.of(x2, y2));
+
+        Set<List<Integer>> histA = new HashSet<>(Set.copyOf(setA));
+        Set<List<Integer>> histB = new HashSet<>(Set.copyOf(setB));
+
+        if (x1 == x2 && y1 == y2) {
+            return 0;
+        }
+
+        int time = 1;
+        while (true) {
+            Set<List<Integer>> newSetA = new HashSet<>();
+            Set<List<Integer>> newSetB = new HashSet<>();
+
+            setA.forEach(posA -> {
+                newSetA.add(List.of(posA.get(0) + 1, posA.get(1)));
+                newSetA.add(List.of(posA.get(0) , posA.get(1) + 1));
+                newSetA.add(List.of(posA.get(0) - 1, posA.get(1)));
+                newSetA.add(List.of(posA.get(0) , posA.get(1) - 1));
+            });
+
+            setB.forEach(posB -> {
+                newSetB.add(List.of(posB.get(0) + 1, posB.get(1) + 1));
+                newSetB.add(List.of(posB.get(0) + 1, posB.get(1) - 1));
+                newSetB.add(List.of(posB.get(0) - 1, posB.get(1) + 1));
+                newSetB.add(List.of(posB.get(0) - 1, posB.get(1) - 1));
+            });
+
+            newSetA.removeAll(histA);
+            newSetB.removeAll(histB);
+
+            for(List<Integer> posA : newSetA) {
+                if (newSetB.contains(posA)) {
+                    return time;
+                }
+            }
+
+            histA.addAll(newSetA);
+            histB.addAll(newSetB);
+
+            setA = newSetA;
+            setB = newSetB;
+            time++;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.solution(2, 4, 5, -3));
+    }
+
+}
