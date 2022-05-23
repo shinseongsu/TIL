ed42bdb9d8df8ab4c27106910c86f38eeea7f5f9
Shin seongsu <tlstjdtn321@naver.com>
2022-05-23 22:41:49 +0900

    :rocket: middle 넣기 추가

---
+++ 자료구조-알고리즘/선형 자료구조/데크 문제풀이/src/Pratice3.java
@@ -0,0 +1,125 @@
+class MyDeque {
+    int[] arr;
+    int front = 0;
+    int rear = 0;
+
+    MyDeque(int size) {
+        this.arr = new int[size + 1];
+    }
+
+    public boolean isEmpty() {
+        return this.rear == this.front;
+    }
+
+    public boolean isFull() {
+        return (this.rear + 1)  % this.arr.length == this.front;
+    }
+
+    public void addFirst(int data) {
+        if (this.isFull()) {
+            System.out.println("Deque is full!");
+            return;
+        }
+
+        this.arr[front] = data;
+        this.front = (this.front - 1 + this.arr.length) % this.arr.length;
+    }
+
+    public void addLast(int data) {
+        if (this.isFull()) {
+            System.out.println("Deque is full!");
+            return;
+        }
+
+        this.rear = (this.rear + 1) % this.arr.length;
+        this.arr[this.rear] = data;
+    }
+
+    public void addMiddle(int data) {
+        if (this.isFull()) {
+            System.out.println("Deque is full!");
+            return;
+        }
+
+        int elements = this.rear - this.front;
+        if (elements < 0) {
+            elements = this.arr.length + elements;
+        }
+
+        int mid = (this.rear - elements / 2 + this.arr.length) % this.arr.length + 1;
+
+        int start = (this.rear + 1) % this.arr.length;
+        int end = (this.rear - elements /2 + this.arr.length) % this.arr.length;
+        for (int i = start; i != end; i = (i - 1 + this.arr.length) % this.arr.length) {
+            this.arr[i] = this.arr[(i - 1 + this.arr.length) % this.arr.length];
+        }
+        this.arr[mid] = data;
+        this.rear = (this.rear + 1) % this.arr.length;
+    }
+
+    public Integer removeFirst() {
+        if (this.isEmpty()) {
+            System.out.println("Deque is empty!");
+            return null;
+        }
+
+        this.front = (this.front + 1) % this.arr.length;
+        return this.arr[this.front];
+    }
+
+    public Integer removeLast() {
+        if (this.isEmpty()) {
+            System.out.println("Deque is empty!");
+            return null;
+        }
+
+        int data = this.arr[this.rear];
+        this.rear = (this.rear - 1 + this.arr.length) % this.arr.length;
+        return data;
+    }
+
+    public void printDeque() {
+        int start = (this.front + 1) % this.arr.length;
+        int end = (this.rear + 1) % this.arr.length;
+
+        for (int i = start; i != end; i = (i +1) % this.arr.length) {
+            System.out.print(this.arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
+
+public class Pratice3 {
+    public static void main(String[] args) {
+        MyDeque myDeque1 = new MyDeque(5);
+        myDeque1.addLast(1);
+        myDeque1.addLast(2);
+        myDeque1.addLast(3);
+        myDeque1.addLast(4);
+        myDeque1.printDeque();
+
+        myDeque1.addMiddle(10);
+        myDeque1.printDeque();
+
+        MyDeque myDeque2 = new MyDeque(5);
+        myDeque2.addLast(10);
+        myDeque2.addLast(10);
+        myDeque2.addLast(10);
+        myDeque2.addLast(10);
+        myDeque2.addLast(10);
+        myDeque2.removeFirst();
+        myDeque2.removeFirst();
+        myDeque2.removeFirst();
+        myDeque2.removeFirst();
+        myDeque2.addLast(11);
+        myDeque2.addLast(12);
+        myDeque2.addLast(13);
+        myDeque2.printDeque();
+
+        myDeque2.addMiddle(100);
+        myDeque2.printDeque();
+
+
+    }
+}
