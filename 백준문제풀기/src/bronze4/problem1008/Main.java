aec3abf87f399febe821c2361e7c93a7f1f87989
Shin seongsu <tlstjdtn321@naver.com>
2022-05-24 23:23:34 +0900

    :fire: A/B

---
+++ 백준문제풀기/src/bronze4/problem1008/Main.java
@@ -0,0 +1,13 @@
+package bronze4.problem1008;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.println(a / b);
+    }
+}
