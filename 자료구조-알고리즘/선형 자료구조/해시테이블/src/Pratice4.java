7ca9ca1b73b682b286805ebba12ec92d26d57200
Shin seongsu <tlstjdtn321@naver.com>
2022-05-28 23:19:29 +0900

    :rocket: 이중 해싱

--- 자료구조-알고리즘/선형 자료구조/해시테이블/src/Pratice4.java
+++ 자료구조-알고리즘/선형 자료구조/해시테이블/src/Pratice4.java
@@ -1,2 +1,76 @@
-package PACKAGE_NAME;public class Pratice4 {
+class MyHashTable4 extends MyHashTable {
+    int c;
+
+    MyHashTable4(int size) {
+        super(size);
+        this.c = this.getHashC(size);
+    }
+
+    public int getHashC(int size) {
+        int c = 0;
+
+        if (size <= 2) {
+            return size;
+        }
+
+        for (int i = size-1; i > 2; i--) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                c = i;
+                break;
+            }
+        }
+        return c;
+    }
+
+    public int getHash2(int key) {
+        int hash = 1 + key % this.c;
+        return hash;
+    }
+
+    public void setValue(int key, int data) {
+        int idx = this.getHash(key);
+
+        if (this.elemCnt == this.table.length) {
+            System.out.println("Hash table is full!");
+            return;
+        } else if (this.table[idx] == null) {
+            this.table[idx] = data;
+        } else {
+            int newIdx = idx;
+            int cnt = 1;
+            while (true) {
+                newIdx = (newIdx + this.getHash2(newIdx) * cnt) % this.table.length;
+                if (this.table[newIdx] == null) {
+                    break;
+                }
+                cnt++;
+            }
+            this.table[newIdx] = data;
+        }
+        this.elemCnt++;
+    }
+
+}
+
+public class Pratice4 {
+    public static void main(String[] args) {
+        MyHashTable4 ht = new MyHashTable4(11);
+        ht.setValue(1, 10);
+        ht.setValue(2, 20);
+        ht.setValue(3, 30);
+        ht.printHashTable();
+
+        ht.setValue(1, 100);
+        ht.setValue(1, 200);
+        ht.setValue(1, 300);
+        ht.printHashTable();
+    }
+
 }
