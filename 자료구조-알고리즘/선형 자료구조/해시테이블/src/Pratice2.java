96eb981d25ced40bbb62d82a4114fe89eaf63534
Shin seongsu <tlstjdtn321@naver.com>
2022-05-28 23:18:09 +0900

    :rocket: 선형 탐사법

--- 자료구조-알고리즘/선형 자료구조/해시테이블/src/Pratice2.java
+++ 자료구조-알고리즘/선형 자료구조/해시테이블/src/Pratice2.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Pratice2 {
+
+class MyHashTable2 extends MyHashTable {
+
+    MyHashTable2(int size) {
+        super(size);
+    }
+
+    public void setValue(int key, int data) {
+        int idx = this.getHash(key);
+
+        if(this.elemCnt == this.table.length) {
+            System.out.println("Hash Table is full!");
+            return;
+        } else if (this.table[idx] == null ) {
+            this.table[idx] = data;
+        } else {
+            int newIdx = idx;
+            while (true) {
+                newIdx = (newIdx + 1) % this.table.length;
+                if (this.table[newIdx] == null) {
+                    break;
+                }
+            }
+            this.table[newIdx] = data;
+        }
+        elemCnt++;
+    }
+}
+
+public class Pratice2 {
+    public static void main(String[] args) {
+        MyHashTable2 ht = new MyHashTable2(5);
+        ht.setValue(1, 1);
+        ht.setValue(3, 3);
+        ht.printHashTable();
+
+        ht.setValue(1, 10);
+        ht.printHashTable();
+
+        ht.setValue(1, 20);
+        ht.setValue(1, 30);
+        ht.setValue(1, 40);
+        ht.printHashTable();
+
+    }
 }
