26f9822acf604fc9fedce341ae1650b60a384356
Shin seongsu <tlstjdtn321@naver.com>
2022-05-28 23:19:05 +0900

    :rocket: 제곱 탐사법

--- 자료구조-알고리즘/선형 자료구조/해시테이블/src/Pratice3.java
+++ 자료구조-알고리즘/선형 자료구조/해시테이블/src/Pratice3.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class MyHashTable3 {
+class MyHashTable3 extends MyHashTable {
+
+    MyHashTable3(int size) {
+        super(size);
+    }
+
+    public void setValue(int key, int data) {
+        int idx = this.getHash(key);
+
+        if (this.elemCnt == this.table.length) {
+            System.out.println("Hash table is full!");
+        } else if (this.table[idx] == null) {
+            this.table[idx] = data;
+        } else {
+            int newIdx = idx;
+            int cnt = 0;
+            while (true) {
+                newIdx = (newIdx + (int) Math.pow(2, cnt)) % this.table.length;
+                if (this.table[newIdx] == null) {
+                    break;
+                }
+                cnt++;
+            }
+            this.table[newIdx] = data;
+        }
+        this.elemCnt++;
+    }
+
+}
+
+public class Pratice3 {
+    public static void main(String[] args) {
+        MyHashTable3 ht = new MyHashTable3(11);
+        ht.setValue(1, 10);
+        ht.setValue(2, 20);
+        ht.setValue(4, 40);
+        ht.printHashTable();
+
+        ht.setValue(1, 100);
+        ht.printHashTable();
+
+        ht.setValue(1, 200);
+        ht.setValue(1, 300);
+        ht.setValue(1, 400);
+        ht.printHashTable();
+    }
 }
