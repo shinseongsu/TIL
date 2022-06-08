cee3fb77144357ddb5b282a4fdb1f22ae676b2cf
Shin seongsu <tlstjdtn321@naver.com>
2022-06-08 23:54:52 +0900

    anchor: 코프링 기본 셋업

---
+++ kotlinSpringBoot/src/main/kotlin/com/kotlinspring/kotlinspringboot/KotlinSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlinspring.kotlinspringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBootApplication>(*args)
+}
---
+++ kotlinSpringBoot/src/test/kotlin/com/kotlinspring/kotlinspringboot/KotlinSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinspring.kotlinspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
