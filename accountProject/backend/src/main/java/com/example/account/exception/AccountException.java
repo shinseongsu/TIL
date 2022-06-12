5394126226933b4610a8c9124928340a87493b6e
Shin seongsu <tlstjdtn321@naver.com>
2022-06-12 23:54:21 +0900

    feat: 공통 예외 추가

---
+++ accountProject/backend/src/main/java/com/example/account/exception/AccountException.java
@@ -0,0 +1,9 @@
+package com.example.account.exception;
+
+public class AccountException extends RuntimeException {
+
+    public AccountException(String message) {
+        super(message);
+    }
+
+}
