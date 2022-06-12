c4a844fd0fb5f201d84d50333a10ae202a44d2be
Shin seongsu <tlstjdtn321@naver.com>
2022-06-12 23:53:18 +0900

    feat: entity 추가

---
+++ accountProject/backend/src/main/java/com/example/account/domain/base/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.example.account.domain.base;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.Modifying;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public abstract class BaseEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    private String createBy;
+
+    @LastModifiedBy
+    private String modifiedBy;
+
+}
