00d6983716a806e2e095de028ee9a360a9e64bf9
Shin seongsu <tlstjdtn321@naver.com>
2022-07-06 22:43:54 +0900

    chore: 테스트 및 빌드 추가

---
+++ 계좌 시스템/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into account_user(id, name, created_at, updated_at)
+values (1, 'Prooro', now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (2, 'Lupi', now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (3, 'Eddie', now(), now());
