3bf91de74ab4196cec7fe703d24a59a937d12c1a
Shin seongsu <tlstjdtn321@naver.com>
2022-12-18 20:53:35 +0900

    :sparkles: bookingEntity 추가

---
+++ healthSpringBatch/src/main/java/com/example/pass/repository/booking/BookingEntity.java
@@ -0,0 +1,43 @@
+package com.example.pass.repository.booking;
+
+import com.example.pass.repository.BaseEntity;
+import com.example.pass.repository.pass.PassEntity;
+import com.example.pass.repository.user.UserEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "booking")
+public class BookingEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bookingSeq;
+    private Integer passSeq;
+    private String userId;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+    private boolean usedPass;
+    private boolean attended;
+
+    private LocalDateTime startedAt;
+    private LocalDateTime endedAt;
+    private LocalDateTime cancelledAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", insertable = false, updatable = false)
+    private UserEntity userEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "passSeq", insertable = false, updatable = false)
+    private PassEntity passEntity;
+
+}
---
+++ healthSpringBatch/src/main/java/com/example/pass/repository/booking/BookingStatus.java
@@ -0,0 +1,5 @@
+package com.example.pass.repository.booking;
+
+public enum BookingStatus {
+    READY, PROGRESSED, COMPLETED, CANCELLED
+}
