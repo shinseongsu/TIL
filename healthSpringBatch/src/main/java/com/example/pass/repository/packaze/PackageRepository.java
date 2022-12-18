e26d2c65a2c4e100f39b381a7a2ba47392e75cbf
Shin seongsu <tlstjdtn321@naver.com>
2022-12-18 20:55:18 +0900

    :sparkles: 패키지 레포지토리 추가

--- healthSpringBatch/src/main/java/com/example/pass/repository/packaze/PackageRepository.java
+++ healthSpringBatch/src/main/java/com/example/pass/repository/packaze/PackageRepository.java
@@ -1,6 +1,24 @@
 package com.example.pass.repository.packaze;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface PackageRepository extends JpaRepository<PackageEntity, Integer> {
+
+    List<PackageEntity> findByCreatedAtAfter(LocalDateTime dateTime, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE PackageEntity p " +
+                   "   SET p.count = :count, " +
+                   "       p.period = :period  " +
+                   " WHERE p.packageSeq = :packageSeq "
+    )
+    int updateCountAndPeriod(Integer packageSeq, int count, int period);
 }
