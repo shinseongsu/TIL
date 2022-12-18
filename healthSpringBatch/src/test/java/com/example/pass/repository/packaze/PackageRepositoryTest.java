954d290d9fe8f11fc3e90fe942c7802b5f874bb4
Shin seongsu <tlstjdtn321@naver.com>
2022-12-18 20:55:45 +0900

    :white_check_mark: repository 테스트 코드 추가

--- healthSpringBatch/src/test/java/com/example/pass/repository/packaze/PackageRepositoryTest.java
+++ healthSpringBatch/src/test/java/com/example/pass/repository/packaze/PackageRepositoryTest.java
@@ -1,10 +1,19 @@
 package com.example.pass.repository.packaze;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
 @SpringBootTest
 @ActiveProfiles("test")
 class PackageRepositoryTest {
@@ -18,7 +27,64 @@ class PackageRepositoryTest {
         packageEntity.setPackageName("바디 챌리지 PT 12주");
         packageEntity.setPeriod(84);
 
+        packageRepository.save(packageEntity);
 
+        assertNotNull(packageEntity.getPackageSeq());
+    }
+
+    @Test
+    void testFindByCreatedAfter() {
+        LocalDateTime dateTime = LocalDateTime.now().minusMinutes(1);
+
+        PackageEntity packageEntity0 = new PackageEntity();
+        packageEntity0.setPackageName("학생 전용 3개월");
+        packageEntity0.setPeriod(90);
+        packageRepository.save(packageEntity0);
+
+        PackageEntity packageEntity1 = new PackageEntity();
+        packageEntity1.setPackageName("학생 전용 6개월");
+        packageEntity1.setPeriod(180);
+        packageRepository.save(packageEntity1);
+
+        // when
+        final List<PackageEntity> packageEntities = packageRepository.findByCreatedAtAfter(dateTime, PageRequest.of(0, 1, Sort.by("packageSeq").descending()));
+
+        // then
+        assertEquals(1, packageEntities.size());
+        assertEquals(packageEntity1.getPackageSeq(), packageEntities.get(0).getPackageSeq());
+    }
+
+    @Test
+    void testUpdateCountAndPeriod() {
+        // given
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("바디프로필 이벤트 4개월");
+        packageEntity.setPeriod(90);
+        packageRepository.save(packageEntity);
+
+        // when
+        int updateCount = packageRepository.updateCountAndPeriod(packageEntity.getPackageSeq(), 30, 120);
+        final PackageEntity updatedPackageEntity = packageRepository.findById(packageEntity.getPackageSeq()).get();
+
+        // then
+        assertEquals(1, updateCount);
+        assertEquals(30, updatedPackageEntity.getCount());
+        assertEquals(120, updatedPackageEntity.getPeriod());
+    }
+
+    @Test
+    void testDelete() {
+        // given
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("제거할 이용권");
+        packageEntity.setCount(1);
+        PackageEntity newPacakgeEntity = packageRepository.save(packageEntity);
+
+        // when
+        packageRepository.deleteById(newPacakgeEntity.getPackageSeq());
+
+        // then
+        assertTrue(packageRepository.findById(newPacakgeEntity.getPackageSeq()).isEmpty());
     }
 
 }
