041bf5f58c12493bd7dc72a5b19be907da6827ea
Shin seongsu <tlstjdtn321@naver.com>
2023-01-07 23:50:21 +0900

    grpc init

---
+++ gRpcApplication/src/main/java/com/example/grpcapplication/GRpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.grpcapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GRpcApplication.class, args);
+    }
+
+}
---
+++ gRpcApplication/src/test/java/com/example/grpcapplication/GRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpcapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
