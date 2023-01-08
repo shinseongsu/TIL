98510da5adbf170bb50d52ca7c6f4ecdbf6ca761
Shin seongsu <tlstjdtn321@naver.com>
2023-01-08 23:32:02 +0900

    :sparkles: Product entity 추가

---
+++ gRpcApplication/src/main/java/com/example/grpcapplication/domain/Product.java
@@ -0,0 +1,42 @@
+package com.example.grpcapplication.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private Integer quantityInStock;
+
+    protected Product() {
+    }
+
+    public Product(Long id, String name, Double price, Integer quantityInStock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantityInStock = quantityInStock;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Integer getQuantityInStock() {
+        return quantityInStock;
+    }
+}
