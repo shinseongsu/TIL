b8b2118bf7247b4d6a3bcc6a711ffd387e9860cf
Shin seongsu <tlstjdtn321@naver.com>
2023-02-20 21:28:21 +0900

    feat: userGroupMapping entity, repository 추가

---
+++ healthSpringBatch/src/main/java/com/example/pass/repository/user/UserGroupMappingEntity.java
@@ -0,0 +1,30 @@
+package com.example.pass.repository.user;
+
+import com.example.pass.repository.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "user_group_mapping")
+@IdClass(UserGroupMappingId.class)
+public class UserGroupMappingEntity extends BaseEntity {
+
+    @Id
+    private String userGroupId;
+
+    @Id
+    private String userId;
+
+    private String userGroupName;
+    private String description;
+
+}
---
+++ healthSpringBatch/src/main/java/com/example/pass/repository/user/UserGroupMappingRepository.java
@@ -0,0 +1,9 @@
+package com.example.pass.repository.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserGroupMappingRepository extends JpaRepository<UserGroupMappingEntity, Integer> {
+    List<UserGroupMappingEntity> findByUserGroupId(String userGroupId);
+}
