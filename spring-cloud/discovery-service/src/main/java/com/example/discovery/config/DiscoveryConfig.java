7958248a68896a999cfaad84fdbb2aef75673aad
Shin seongsu <tlstjdtn321@naver.com>
2023-03-26 13:15:05 +0900

    feat: 유레카 서버 만들어보기

---
+++ spring-cloud/discovery-service/src/main/java/com/example/discovery/config/DiscoveryConfig.java
@@ -0,0 +1,9 @@
+package com.example.discovery.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class DiscoveryConfig {
+}
---
+++ spring-cloud/discovery-service/src/test/java/com/example/discovery/DiscovertyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscovertyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
