9b9a1e04221155ca4f26514fc1034c455a921faa
Shin seongsu <tlstjdtn321@naver.com>
2023-07-03 23:51:39 +0900

    feat: MSA 기초 공부

---
+++ fastcamPay/src/test/java/com/example/fastcampay/FastcamPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastcampay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcamPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
