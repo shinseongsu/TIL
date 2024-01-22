d9e675a9f6fd8b7275aae4e0c32459fe1bde0bc1
Shin seongsu <tlstjdtn321@naver.com>
2024-01-22 23:37:40 +0900

    feat: main application 추가

---
+++ kotlin-web/portfolio/src/main/kotlin/com/example/portfolio/PortfolioApplication.kt
@@ -0,0 +1,11 @@
+package com.example.portfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioApplication
+
+fun main(args: Array<String>) {
+    runApplication<PortfolioApplication>(*args)
+}
