bd6ac5f089998b7f40533655a5563b1280d4f883
Shin seongsu <tlstjdtn321@naver.com>
2024-01-31 00:12:15 +0900

    feat: repository 기능 추가

--- kotlin-web/portfolio/src/main/kotlin/com/example/portfolio/domain/repository/ExperienceRepository.kt
+++ kotlin-web/portfolio/src/main/kotlin/com/example/portfolio/domain/repository/ExperienceRepository.kt
@@ -2,7 +2,9 @@ package com.example.portfolio.domain.repository
 
 import com.example.portfolio.domain.entity.Achievement
 import com.example.portfolio.domain.entity.Experience
+import com.example.portfolio.domain.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ExperienceRepository: JpaRepository<Experience, Long> {
+    fun findAllByIsActive(isActive: Boolean): List<Experience>
 }
