69b2fc7f014bfcb1ea8735a8bac35e117a26e334
Shin seongsu <tlstjdtn321@naver.com>
2024-01-31 00:11:42 +0900

    fix: fetch 조건 수정

--- kotlin-web/portfolio/src/main/kotlin/com/example/portfolio/domain/entity/Project.kt
+++ kotlin-web/portfolio/src/main/kotlin/com/example/portfolio/domain/entity/Project.kt
@@ -48,7 +48,7 @@ class Project(
     @JoinColumn(name = "project_id")
     var details: MutableList<ProjectDetail> = mutableListOf()
 
-    @OneToMany(mappedBy = "project")
+    @OneToMany(mappedBy = "project", fetch = FetchType.LAZY, cascade = [CascadeType.PERSIST])
     var skills: MutableList<ProjectSkill> = mutableListOf()
 
 
