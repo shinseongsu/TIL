d6e954ea59306a2f1ecaa5aa64869856db125c7b
Shin seongsu <tlstjdtn321@naver.com>
2024-02-06 21:23:43 +0900

    feat: interceptor 추가하여 요청건들 저장하기

--- kotlin-web/portfolio/src/main/kotlin/com/example/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ kotlin-web/portfolio/src/main/kotlin/com/example/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,4 +1,25 @@
 package com.example.portfolio.presentation.interceptor
 
-class PresentationInterceptor {
+import com.example.portfolio.domain.entity.HttpInterface
+import com.example.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
+
+@Component
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+): HandlerInterceptor {
+
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterceptor = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterceptor)
+    }
 }
--- kotlin-web/portfolio/src/main/kotlin/com/example/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
+++ kotlin-web/portfolio/src/main/kotlin/com/example/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
@@ -1,7 +1,18 @@
 package com.example.portfolio.presentation.interceptor
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class PresentationInterceptorConfiguration {
+class PresentationInterceptorConfiguration(
+    private val presentationInterceptor: PresentationInterceptor
+): WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(presentationInterceptor)
+            .addPathPatterns("/**")
+            .excludePathPatterns("/assets/**", "/css/**", "/js/**", "/admin/**", "h2**", "/favicon.ico", "/error")
+    }
+
 }
